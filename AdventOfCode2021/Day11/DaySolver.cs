d6e581d3e0e62f1a428db3b8855872c9e3f1f2ff
Evgeny Buzyuma <ebuzyuma@gmail.com>
2021-12-11 06:42:06 +0100

    Added day 11

---
+++ AdventOfCode2021/Day11/DaySolver.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventOfCode2021.Day11
+{
+    public class DaySolver : SolverBase
+    {
+        protected override string DayNo => "11";
+
+        protected override string Solve(string[] input)
+        {
+            var grid = input.Select(x => x.Select(y => int.Parse(y.ToString())).ToList()).ToList();
+            var nextState = new List<List<int>>();
+
+            var neighbours = new List<(int, int)>
+            {
+                (-1, -1),
+                (-1, 0),
+                (-1, +1),
+                (0, -1),
+                (0, +1),
+                (1, -1),
+                (1, 0),
+                (1, +1),
+            };
+
+            int n = 0;
+            bool allFlash = false;
+            while (!allFlash)
+            {
+                n++;
+                //Print(grid);
+
+                int total = 0;
+                for (int i = 0; i < grid.Count; i++)
+                {
+                    for (int j = 0; j < grid[i].Count; j++)
+                    {
+                        grid[i][j]++;
+                    }
+                }
+
+                while(grid.SelectMany(x => x).Any(x => x > 9))
+                {
+                    for (int i = 0; i < grid.Count; i++)
+                    {
+                        for (int j = 0; j < grid[i].Count; j++)
+                        {
+                            if (grid[i][j] > 9)
+                            {
+                                total++;
+                                grid[i][j] = 0;
+                                neighbours.ForEach(x => IncreaseNeighbour(grid, i, j, x.Item1, x.Item2));
+                            }
+                        }
+                    }
+                }
+                if (total == 100)
+                {
+                    allFlash = true;
+                }
+            }
+
+            return n.ToString();
+        }
+
+        private void Print(List<List<int>> grid)
+        {
+            for (int i = 0; i < grid.Count; i++)
+            {
+                for (int j = 0; j < grid[i].Count; j++)
+                {
+                    Console.Write(grid[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private void IncreaseNeighbour(List<List<int>> grid, int i, int j, int iDir, int jDir)
+        {
+            if (i + iDir < 0 || j + jDir < 0
+                || i + iDir >= grid.Count || j + jDir >= grid[i].Count)
+            {
+                return;
+            }
+            if (grid[i + iDir][j + jDir] != 0)
+            {
+                grid[i + iDir][j + jDir]++;
+            }
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode2021.Day10;
+﻿using AdventOfCode2021.Day11;
 
 var solver = new DaySolver();
 
