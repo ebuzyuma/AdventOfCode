bb534ed14a3d1ed3a7c63c147cd8e0c2348cf95d
Evgeny Buzyuma <ebuzyuma@gmail.com>
2021-12-14 08:10:45 +0100

    Added day 14

---
+++ AdventOfCode2021/Day14/DaySolver.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+
+namespace AdventOfCode2021.Day14
+{
+    public class DaySolver : SolverBase2
+    {
+        protected override string DayNo => "14";
+
+        protected override (string, string) Solve(string[] input)
+        {
+            string polymer = input[0];
+
+            var rules = input.Skip(2).Select(x => x.Split(" -> "))
+                .ToDictionary(k => k[0], v => v[1]);
+
+            // Part 1
+            var result = BuildPolymer(polymer, rules, 10);
+            var group = result.AsEnumerable().GroupBy(x => x).ToDictionary(x => x.Key, x => x.LongCount());
+            var min = group.Min(x => x.Value);
+            var max = group.Max(x => x.Value);
+            var part1 = max - min;
+
+            // Part 2
+            var histogram = BuildHistogram(polymer, rules, 40);
+            min = histogram.Min(x => x.Value);
+            max = histogram.Max(x => x.Value);
+            var part2 = max - min;
+
+            return (part1.ToString(), part2.ToString());
+        }
+
+        private Dictionary<char, long> BuildHistogram(string polymer, Dictionary<string, string> rules, int iterations)
+        {
+            // idea:
+            // build cache for all rules for half of iterations
+            // Count for half of iterations from polymer and for the other half from cache
+
+            var alphabet = rules.Values.Select(x => x[0]).Union(polymer).Distinct();
+            var histogram = alphabet.ToDictionary(k => k, v => 0L);
+            
+            // Build cache
+            var rulesHistograms = new Dictionary<string, Dictionary<char, long>>();
+            foreach (var rule in rules)
+            {
+                var ruleHistogram = alphabet.ToDictionary(k => k, v => 0L);
+                Count(rule.Key[0], rule.Key[1], 0, iterations / 2, ruleHistogram, rules);
+                rulesHistograms[rule.Key] = ruleHistogram;
+            }
+
+            // COunt for half of iterations
+            ItemsOfPolymer.Clear();
+            for (int i = 0; i < polymer.Length - 1; i++)
+            {
+                Count(polymer[i], polymer[i + 1], 0, iterations / 2, histogram, rules);
+            }
+
+            // Count the rest from cache 
+            foreach (var left in ItemsOfPolymer)
+            {
+                var key = rulesHistograms[$"{left.Item1}{left.Item2}"];
+                foreach (var item in key)
+                {
+                    histogram[item.Key] += item.Value;
+                }
+            }
+
+            histogram[polymer.First()]++;
+            histogram[polymer.Last()]++;
+
+            return histogram;
+        }
+
+        private string BuildPolymer(string polymer, Dictionary<string, string> rules, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                var newPolymer = new StringBuilder();
+                newPolymer.Append(polymer.First());
+                for (int j = 0; j < polymer.Length - 1; j++)
+                {
+                    if (rules.TryGetValue($"{polymer[j]}{polymer[j + 1]}", out var add))
+                    {
+                        newPolymer.Append(add);
+                    }
+                    newPolymer.Append(polymer[j + 1]);
+                }
+                polymer = newPolymer.ToString();
+            }
+
+            return polymer;
+        }
+
+        private List<(char, char)> ItemsOfPolymer = new List<(char, char)>();
+
+        private void Count(char a, char b, int iteration, int maxIterations,
+            Dictionary<char, long> values,
+            Dictionary<string, string> rules)
+        {
+            if (iteration == maxIterations)
+            {
+                ItemsOfPolymer.Add((a, b));
+                return;
+            }
+
+            if (rules.TryGetValue($"{a}{b}", out var add))
+            {
+                values[add[0]]++;
+                Count(a, add[0], iteration + 1, maxIterations, values, rules);
+                Count(add[0], b, iteration + 1, maxIterations, values, rules);                
+            }
+        }
+    }
+
+}
