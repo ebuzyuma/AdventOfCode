9036898cf4ba3301f62e9f2ee64144ce9e79c503
Evgeny Buzyuma <ebuzyuma@gmail.com>
2022-12-12 10:23:47 +0100

    Day 12 refactored a bit

--- AdventOfCode2022/Day12/day12.js
+++ AdventOfCode2022/Day12/day12.js
@@ -8,13 +8,6 @@ const directions = [
   [-1, 0],
 ];
 
-const directionsToL = {
-  "0,1": ">",
-  "0,-1": "<",
-  "1,0": "v",
-  "-1,0": "^",
-};
-
 const charToInt = (ch) => {
   if (ch === "E") {
     return "z".charCodeAt(0);
@@ -25,7 +18,7 @@ const charToInt = (ch) => {
   return ch.charCodeAt(0);
 };
 
-const getPossibleMoves = (grid, visited, [x, y]) => {
+const getPossibleMoves = (grid, [x, y]) => {
   let possible = [];
   let current = charToInt(grid[x][y]);
   directions.forEach(([dx, dy]) => {
@@ -42,35 +35,20 @@ const getPossibleMoves = (grid, visited, [x, y]) => {
   return possible;
 };
 
-const printMap = (map) => {
-  let str = "";
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = 0; j < lines[i].length; j++) {
-      str += map[i][j] || " ";
-    }
-    str += "\n";
-  }
-  console.log(str);
-};
-
-const findMin = (lines, [x0, y0]) => {
-  let visited = [...Array(lines.length)].map((x) => []);
-  let viewMap = [...Array(lines.length)].map((x) => []);
+const findMin = (grid, [x0, y0]) => {
+  let visited = [...Array(grid.length)].map((x) => []);
   let queue = [[x0, y0]];
   visited[x0][y0] = 0;
-  let [xe, ye] = [0, 0];
+  let minPathLength = Number.MAX_SAFE_INTEGER;
   while (queue.length > 0) {
-    //printMap(viewMap);
     let next = new Set();
     queue.forEach(([x, y]) => {
-      const moves = getPossibleMoves(lines, visited, [x, y]);
+      const moves = getPossibleMoves(grid, [x, y]);
       moves.forEach(([x1, y1]) => {
-        if (lines[x1][y1] === "E") {
-          [xe, ye] = [x1, y1];
-          visited[x1][y1] = Math.min(visited[x][y] + 1, visited[x1][y1] || 1000000);
+        if (grid[x1][y1] === "E" && visited[x][y] + 1 < minPathLength) {
+          minPathLength = visited[x][y] + 1;
         } else if (!visited[x1][y1] || visited[x1][y1] > visited[x][y] + 1) {
           visited[x1][y1] = visited[x][y] + 1;
-          viewMap[x1][y1] = directionsToL[[x - x1, y - y1].toString()];
           next.add([x1, y1]);
         }
       });
@@ -79,15 +57,19 @@ const findMin = (lines, [x0, y0]) => {
     queue = [...next.values()];
   }
 
-  return visited[xe][ye];
+  return minPathLength;
 };
 
-let min = 10000000;
+// Part 1
+console.log(findMin(lines, [0, 0]));
+
+// Part 2
+let min = Number.MAX_SAFE_INTEGER;
 for (let i = 0; i < lines.length; i++) {
   for (let j = 0; j < lines[i].length; j++) {
     if (lines[i][j] === "a" || lines[i][j] === "S") {
       let x = findMin(lines, [i, j]);
-      min = x ? Math.min(x, min) : min;
+      min = x < min ? x : min;
     }
   }
 }
