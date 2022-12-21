2798de35db3a563d8399c0d6263a68e25572ee5f
Evgeny Buzyuma <ebuzyuma@gmail.com>
2022-12-21 14:14:44 +0100

    Utils

--- AdventOfCode2022/utils.js
+++ AdventOfCode2022/utils.js
@@ -9,4 +9,17 @@ const readInput = (inputFolder) => {
   return lines;
 };
 
-module.exports = { readInput };
+const readExampleInput = (inputFolder) => {
+  const data = fs.readFileSync(`${inputFolder}/example.txt`, "utf8");
+
+  const lines = data.split("\n").map((x) => x.trim());
+
+  return lines;
+};
+
+const multilineRegex = (...parts) =>
+  new RegExp(parts.map((x) => (x instanceof RegExp ? x.source : x)).join(""));
+
+module.exports = { readInput, readExampleInput, multilineRegex };
+
+
