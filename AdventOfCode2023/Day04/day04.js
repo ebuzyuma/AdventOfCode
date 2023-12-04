4241bf9acee18686ba2ecf7de46b86b38bc1309b
Evgeny Buzyuma <ebuzyuma@gmail.com>
2023-12-04 07:39:56 +0100

    Refactored to use regex

--- AdventOfCode2023/Day04/day04.js
+++ AdventOfCode2023/Day04/day04.js
@@ -2,46 +2,34 @@ const utils = require("../utils");
 const lines = utils.readInput(__dirname);
 
 // Part 1
-let p1 = 0;
 let cards = {};
+const lineRegex = /^Card +(?<cardNumber>\d+): +(?<winStr>[\d ]+) \| (?<ownStr>[\d ]+)$/;
 for (let line of lines) {
-  const [lineCardStr, numbers] = line.split(": ");
-  const lineCard = +lineCardStr.split(" ").last();
-  const [winStr, ownStr] = numbers.split(" | ");
-  const winNumbers = winStr
-    .split(" ")
-    .filter((c) => !!c)
-    .map((c) => +c);
-  const ownNumbers = ownStr
-    .split(" ")
-    .filter((c) => !!c)
-    .map((c) => +c.trim());
+  const match = lineRegex.exec(line);
+  const { cardNumber, winStr, ownStr } = match.groups;
+  const winNumbers = winStr.split(/ +/).map((c) => +c);
+  const ownNumbers = ownStr.split(/ +/).map((c) => +c);
   const intersect = ownNumbers.filter((n) => winNumbers.includes(n));
-  cards[lineCard] = {
-    score: 0,
-    matches: 0,
+  cards[cardNumber] = {
+    matches: intersect.length,
     count: 1,
   };
-  if (intersect.length > 0) {
-    cards[lineCard].matches = intersect.length;
-  }
 }
-p1 = Object.values(cards)
+const totalScore = Object.values(cards)
   .filter((x) => x.matches > 0)
   .map((x) => Math.pow(2, x.matches - 1))
   .sum();
-console.log(p1);
+console.log(totalScore);
 
 // Part 2
-let p2 = 2;
 for (const [card, value] of Object.entries(cards)) {
   for (let i = 1; i <= value.matches; i++) {
     const next = +card + i;
-    if (cards[next]) cards[next].count += value.count;
+    cards[next].count += value.count;
   }
 }
-p2 = Object.values(cards)
+let totalCards = Object.values(cards)
   .map((x) => x.count)
   .sum();
 
-console.log(p2);
+console.log(totalCards);
