5936b2e87c843b2c8ec30902e50258d14bc69e78
Evgeny Buzyuma <ebuzyuma@gmail.com>
2023-12-06 06:43:19 +0100

    Added custom file name to read input

--- AdventOfCode2023/utils.js
+++ AdventOfCode2023/utils.js
@@ -1,16 +1,8 @@
 require("./array");
 const fs = require("fs");
 
-const readInput = (inputFolder, doNotTrim) => {
-  const data = fs.readFileSync(`${inputFolder}/input.txt`, "utf8");
-
-  const lines = data.split("\n").map((x) => doNotTrim ? x : x.trim());
-
-  return lines;
-};
-
-const readExampleInput = (inputFolder, doNotTrim) => {
-  const data = fs.readFileSync(`${inputFolder}/example.txt`, "utf8");
+const readInput = (inputFolder, file = 'input.txt', doNotTrim = false) => {
+  const data = fs.readFileSync(`${inputFolder}/${file}`, "utf8");
 
   const lines = data.split("\n").map((x) => doNotTrim ? x : x.trim());
 
@@ -20,6 +12,6 @@ const readExampleInput = (inputFolder, doNotTrim) => {
 const multilineRegex = (...parts) =>
   new RegExp(parts.map((x) => (x instanceof RegExp ? x.source : x)).join(""));
 
-module.exports = { readInput, readExampleInput, multilineRegex };
+module.exports = { readInput, multilineRegex };
 
 
