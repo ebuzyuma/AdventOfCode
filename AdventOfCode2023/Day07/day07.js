3d6e6007209021a71af562aff3014327f5656c02
Evgeny Buzyuma <ebuzyuma@gmail.com>
2023-12-07 07:16:12 +0100

    Clean up & refactor

--- AdventOfCode2023/Day07/day07.js
+++ AdventOfCode2023/Day07/day07.js
@@ -3,108 +3,79 @@ const exampleInput = utils.readInput(__dirname, "example.txt");
 const puzzleInput = utils.readInput(__dirname, "input.txt");
 const input = puzzleInput;
 
-let hands = input
-  .map((x) => x.split(" "))
-  .map((s) => ({ cards: s[0], bid: +s[1], kind: findKind(s[0]), kind2: findKind2(s[0]) }));
+let cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"].reverse();
+
+let hands = input.map((x) => x.split(" ")).map((s) => ({ cards: s[0], bid: +s[1] }));
 
 // Part 1
-function findKind(card) {
-  let counts = {};
-  for (let i = 0; i < card.length; i++) {
-    let char = card[i];
-    counts[char] ??= 0;
-    counts[char]++;
-  }
-
-  let values = Object.values(counts).numSort().reverse();
-  if (values.includes(5)) return 6;
-  if (values.includes(4)) return 5;
+function countsToRank(values) {
+  if (values[0] === 5) return 6;
+  if (values[0] === 4) return 5;
   if (values[0] === 3 && values[1] === 2) return 4;
-  if (values.includes(3)) return 3;
+  if (values[0] === 3) return 3;
   if (values[0] === 2 && values[1] === 2) return 2;
   if (values[0] === 2) return 1;
   return 0;
 }
+function getType(hand) {
+  let counts = {};
+  for (let card of hand) {
+    counts[card] = (counts[card] || 0) + 1;
+  }
 
-let cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
+  let values = Object.values(counts).numSort().reverse();
+  let rank = countsToRank(values);
+  return rank;
+}
 
-function cardRank(card) {
+function cardRank(card, cards) {
   return cards.indexOf(card);
 }
 
-function compareHands(a, b) {
-  if (a.kind !== b.kind) {
-    return a.kind - b.kind;
+function compareHands(a, b, cards) {
+  if (a.type !== b.type) {
+    return a.type - b.type;
   }
 
   let i = 0;
   while (a.cards[i] === b.cards[i]) i++;
 
-  return cardRank(b.cards[i]) - cardRank(a.cards[i]);
+  return cardRank(a.cards[i], cards) - cardRank(b.cards[i], cards);
 }
 
-let sorted = hands.sort(compareHands);
-for (let hand of hands) {
-  hand.rank = sorted.indexOf(hand) + 1;
+function getScore(hands, cards) {
+  let sorted = hands.sort((a, b) => compareHands(a, b, cards));
+  return sorted.map((x, i) => x.bid * (i + 1)).sum();
 }
 
-let p1 = sorted.map((x) => x.bid * x.rank).sum();
+let p1Hands = hands.map((h) => ({ ...h, type: getType(h.cards) }));
+let p1 = getScore(p1Hands, cards);
 console.log(p1);
 
 // Part 2
-function countsToRank(values) {
-  if (values.includes(5)) return 6;
-  if (values.includes(4)) return 5;
-  if (values[0] === 3 && values[1] === 2) return 4;
-  if (values.includes(3)) return 3;
-  if (values[0] === 2 && values[1] === 2) return 2;
-  if (values[0] === 2) return 1;
-  return 0;
-}
-function findKind2(card) {
+function getType2(hand) {
   let counts = {};
   let jokers = 0;
-  for (let i = 0; i < card.length; i++) {
-    let char = card[i];
-    if (char === "J") {
+  for (let card of hand) {
+    if (card === "J") {
       jokers++;
       continue;
     }
-    counts[char] ??= 0;
-    counts[char]++;
+    counts[card] = (counts[card] || 0) + 1;
   }
 
   let values = Object.values(counts).numSort().reverse();
   if (jokers === 5) {
     values = [5];
   } else {
+    // Add jokers to the maximum
     values[0] += jokers;
   }
   let rank = countsToRank(values);
   return rank;
 }
 
-let cards2 = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"];
-
-function cardRank2(card) {
-  return cards2.indexOf(card);
-}
-
-function compareHands2(a, b) {
-  if (a.kind2 !== b.kind2) {
-    return a.kind2 - b.kind2;
-  }
-
-  let i = 0;
-  while (a.cards[i] === b.cards[i]) i++;
-
-  return cardRank2(b.cards[i]) - cardRank2(a.cards[i]);
-}
-
-let sorted2 = hands.sort(compareHands2);
-for (let hand of hands) {
-  hand.rank2 = sorted2.indexOf(hand) + 1;
-}
-console.log(sorted2);
-let p2 = sorted2.map((x) => x.bid * x.rank2).sum();
+let cards2 = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"].reverse();
+let p2Hands = hands.map((h) => ({ ...h, type: getType2(h.cards) }));
+let p2 = getScore(p2Hands, cards2);
 console.log(p2);
