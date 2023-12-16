6d9fb24c17be5eb85a7ec5e6e4b731befd32690f
Evgeny Buzyuma <ebuzyuma@gmail.com>
2023-12-16 07:52:02 +0100

    Clean up and refacroring

--- AdventOfCode2023/Day16/day16.js
+++ AdventOfCode2023/Day16/day16.js
@@ -2,21 +2,23 @@ const utils = require("../utils");
 const exampleInput = utils.readInput(__dirname, "example.txt");
 const puzzleInput = utils.readInput(__dirname, "input.txt");
 
-function getKey(row, col) {
+function getKey({ row, col }) {
   return `${row}x${col}`;
 }
 
-function setValue(grid, row, col, value) {
-  grid[getKey(row, col)] = value;
+function getValue(grid, pos) {
+  return grid[getKey(pos)];
 }
 
-function getValue(grid, row, col) {
-  return grid[getKey(row, col)];
+function addValue(grid, pos, dirValue) {
+  let key = getKey(pos);
+  grid[key] = grid[key] || [];
+  grid[key].push(dirToStr(dirValue));
 }
 
-function addValue(grid, row, col, value) {
-  grid[getKey(row, col)] = grid[getKey(row, col)] || [];
-  grid[getKey(row, col)].push(value);
+function hasValue(grid, pos, value) {
+  let arr = grid[getKey(pos)] || [];
+  return arr.includes(dirToStr(value));
 }
 
 function move(pos, dir) {
@@ -28,7 +30,7 @@ function print(grid, visited) {
     let rowStr = "";
     for (let col = 0; col < grid[row].length; col++) {
       let element = grid[row][col];
-      let curVisited = getValue(visited, row, col);
+      let curVisited = getValue(visited, { row, col });
       if (element === "." && curVisited) {
         let str = curVisited.length === 1 ? curVisited[0] : curVisited.length;
         rowStr += str;
@@ -39,7 +41,7 @@ function print(grid, visited) {
     console.log(rowStr);
   }
   console.log("");
-  console.log("            ");
+  console.log("");
 }
 
 function dirToStr({ row, col }) {
@@ -49,129 +51,92 @@ function dirToStr({ row, col }) {
   if (row === 1 && col === 0) return "v";
 }
 
-function startBeam(grid, rows, cols, start, dir, visited) {
-  // BFS
+function runBeam(grid, start, dir) {
+  let rows = grid.length;
+  let cols = grid[0].length;
   let visitedPositions = {};
   let toVisit = [{ pos: start, dir }];
   while (toVisit.length > 0) {
     // print(grid, visitedPositions);
     let curr = toVisit.pop();
-    visited.push(JSON.stringify(curr));
-    addValue(visitedPositions, curr.pos.row, curr.pos.col, dirToStr(curr.dir));
+    addValue(visitedPositions, curr.pos, curr.dir);
     let currValue = grid[curr.pos.row][curr.pos.col];
-    let next = [];
+    let nextDirs = [];
     if (currValue === ".") {
-      let nextPos = move(curr.pos, curr.dir);
-      next.push({ pos: nextPos, dir: curr.dir });
+      nextDirs.push(curr.dir);
     } else if (currValue === "/") {
       let nextDir = { row: -curr.dir.col, col: -curr.dir.row };
-      let nextMirrorPos = move(curr.pos, nextDir);
-      next.push({ pos: nextMirrorPos, dir: nextDir });
+      nextDirs.push(nextDir);
     } else if (currValue === "\\") {
       let nextDir = { row: curr.dir.col, col: curr.dir.row };
-      let nextMirrorPos = move(curr.pos, nextDir);
-      next.push({ pos: nextMirrorPos, dir: nextDir });
+      nextDirs.push(nextDir);
     } else if (currValue === "-") {
       if (curr.dir.row === 0) {
         // pass through
-        let nextSplitPos = move(curr.pos, curr.dir);
-        next.push({ pos: nextSplitPos, dir: curr.dir });
+        nextDirs.push(curr.dir);
       } else {
         // split
-        let nextSplit = [
+        let splitDirs = [
           { row: 0, col: -1 },
           { row: 0, col: 1 },
-        ].map((d) => ({ pos: move(curr.pos, d), dir: d }));
-        next.push(...nextSplit);
+        ];
+        nextDirs.push(...splitDirs);
       }
     } else if (currValue === "|") {
       if (curr.dir.col === 0) {
         // pass through
-        let nextSplitPos = move(curr.pos, curr.dir);
-        next.push({ pos: nextSplitPos, dir: curr.dir });
+        nextDirs.push(curr.dir);
       } else {
         // split
-        let nextSplit = [
+        let splitDirs = [
           { row: -1, col: 0 },
           { row: 1, col: 0 },
-        ].map((d) => ({ pos: move(curr.pos, d), dir: d }));
-        next.push(...nextSplit);
+        ];
+        nextDirs.push(...splitDirs);
       }
     }
 
+    let next = nextDirs.map((dir) => ({ pos: move(curr.pos, dir), dir }));
     let available = next.filter(
       ({ pos }) => pos.row >= 0 && pos.row < rows && pos.col >= 0 && pos.col < cols
     );
-    let visitAdd = available.filter((x) => !visited.includes(JSON.stringify(x)));
-    toVisit.push(...visitAdd);
+    let notVisited = available.filter((x) => !hasValue(visitedPositions, x.pos, x.dir));
+    toVisit.push(...notVisited);
   }
 
   return visitedPositions;
+}
+
+function countEnergized(visited) {
+  return Object.keys(visited).length;
 }
 
 function solve(input) {
   let grid = input.map((r) => r.split(""));
 
   // Part 1
-  let visited = [];
-  let position = startBeam(
-    grid,
-    input.length,
-    input[0].length,
-    { row: 0, col: 0 },
-    { row: 0, col: 1 },
-    visited
-  );
+  let visited = runBeam(grid, { row: 0, col: 0 }, { row: 0, col: 1 });
+  let p1 = countEnergized(visited);
 
+  let rows = grid.length;
+  let cols = grid[0].length;
   let max = 0;
-  for (let row = 0; row < grid.length; row++) {
-    console.log(row);
-    let x1 = startBeam(
-      grid,
-      input.length,
-      input[0].length,
-      { row, col: 0 },
-      { row: 0, col: 1 },
-      []
-    );
+  for (let row = 0; row < rows; row++) {
+    let leftToRight = runBeam(grid, { row, col: 0 }, { row: 0, col: 1 });
 
-    let x2 = startBeam(
-      grid,
-      input.length,
-      input[0].length,
-      { row, col: input[0].length - 1 },
-      { row: 0, col: -1 },
-      []
-    );
-    max = Math.max(max, Object.keys(x1).length, Object.keys(x2).length);
+    let rightToLeft = runBeam(grid, { row, col: cols - 1 }, { row: 0, col: -1 });
+    max = Math.max(max, countEnergized(leftToRight), countEnergized(rightToLeft));
   }
 
-  for (let col = 0; col < grid[0].length; col++) {
-    console.log(col);
-    let x1 = startBeam(
-      grid,
-      input.length,
-      input[0].length,
-      { row: 0, col },
-      { row: 1, col: 0 },
-      []
-    );
+  for (let col = 0; col < cols; col++) {
+    let topToBottom = runBeam(grid, { row: 0, col }, { row: 1, col: 0 });
 
-    let x2 = startBeam(
-      grid,
-      input.length,
-      input[0].length,
-      { row: input.length - 1, col: input[0].length - 1 },
-      { row: -1, col: 0 },
-      []
-    );
-    max = Math.max(max, Object.keys(x1).length, Object.keys(x2).length);
+    let bottomToTop = runBeam(grid, { row: rows - 1, col }, { row: -1, col: 0 });
+    max = Math.max(max, countEnergized(topToBottom), countEnergized(bottomToTop));
   }
-
-  let p1 = visited.length;
 
   // Part 2
-  let p2 = Object.keys(position).length;
+  let p2 = Object.keys(visited).length;
 
   return [p1, p2, max];
 }
