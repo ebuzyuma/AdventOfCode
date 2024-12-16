259dc924f23659a477e7e896760eedcc4d63f195
Evgeny Buzyuma <ebuzyuma@gmail.com>
2024-12-16 09:50:40 +0100

    Day 16 solution

--- AdventOfCode2024/Day16/day16.mjs
+++ AdventOfCode2024/Day16/day16.mjs
@@ -48,22 +48,27 @@ function solve(input, xMax, yMax) {
   const dfs = (map, pos) => {
     // east is starting point
     let end = [];
-    let q = [{ pos, dir: 0, score: 0, from: undefined }];
+    let q = [{ pos, dir: 0, score: 0, from: undefined, paths: [[pos]] }];
     let visited = {};
-    // setValue(visited, ...pos, { score: 0, paths: [[pos]] });
+    const vKey = (r, c, d) => `${r}x${c}:${d}`;
     while (q.length > 0) {
       const cur = q.shift();
       const curValue = getValue(map, ...cur.pos);
-      const vValue = getValue(visited, ...cur.pos);
+      const vValue = visited[vKey(...cur.pos, cur.dir)];
 
       // came to visited from another path with the same score
       if (vValue) {
         if (vValue.score === cur.score) {
           vValue.from.push(cur.from);
+          vValue.paths.push(...cur.paths);
         }
         continue;
       } else {
-        setValue(visited, ...cur.pos, { score: cur.score, from: [cur.from] });
+        visited[vKey(...cur.pos, cur.dir)] = {
+          score: cur.score,
+          from: [cur.from],
+          paths: cur.paths,
+        };
       }
 
       if (curValue === "E") {
@@ -71,10 +76,10 @@ function solve(input, xMax, yMax) {
         continue;
       }
 
-      const iDirs = [cur.dir, cur.dir - 1, cur.dir + 1, cur.dir + 2]
+      const iDirs = [cur.dir, cur.dir - 1, cur.dir + 1]
         .map((d) => (d < 0 ? d + dirs.length : d))
         .map((d) => d % dirs.length);
-      const prices = [0, rotateP, rotateP, rotateP * 2];
+      const prices = [0, rotateP, rotateP];
       for (let i = 0; i < iDirs.length; i++) {
         let [dr, dc] = dirs[iDirs[i]];
         const [nr, nc] = [cur.pos[0] + dr, cur.pos[1] + dc];
@@ -84,7 +89,7 @@ function solve(input, xMax, yMax) {
           continue;
         }
 
-        const vValue = getValue(visited, nr, nc);
+        const vValue = visited[vKey(nr, nc, iDirs[i])];
         const nScore = cur.score + 1 + prices[i];
         if (vValue && nScore > vValue.score) {
           continue;
@@ -95,30 +100,47 @@ function solve(input, xMax, yMax) {
           pos: [nr, nc],
           dir: iDirs[i],
           score: cur.score + 1 + prices[i],
+          paths: cur.paths.map((p) => [...p, [nr, nc]]),
         });
       }
 
       q.orderBy((x) => x.score);
     }
 
-    return [visited, end];
+    let best = Object.keys(visited)
+      .filter((k) => k.startsWith(key(...end)))
+      .orderBy((x) => visited[x].score)[0];
+    let bestPath = visited[best];
+
+    return [bestPath, visited];
   };
 
-  const [visited, end] = dfs(map, reindeer);
+  const [best, visited] = dfs(map, reindeer);
 
-  const p1 = getValue(visited, ...end).score;
+  const p1 = best.score;
 
   // Part 2
   let seats = new Set();
-  let q = [end];
+  let q = best.paths[0].map((p) => key(...p));
   while (q.length > 0) {
-    let cur = q.shift();
-    if (!cur) continue;
-    seats.add(key(...cur));
-    let next = getValue(visited, ...cur).from;
-    q.push(...next);
+    const cur = q.pop();
+    if (seats.has(cur)) {
+      continue;
+    }
+
+    seats.add(cur);
+    // possible paths to the point
+    let bestPathKey = Object.keys(visited)
+      .filter((k) => k.startsWith(cur))
+      .orderBy((x) => visited[x].score)[0];
+    let best = visited[bestPathKey];
+    let qSet = new Set(q);
+    for (let path of best.paths) {
+      const points = path.map((p) => key(...p)).filter((p) => !qSet.has(p) && !seats.has(p));
+      q.push(...points);
+    }
   }
-  let p2 = seats.size;
+  const p2 = seats.size;
 
   return [p1, p2];
 }
