38c13a3ad73ce3f402b196853d51cf60a0d05bfb
Evgeny Buzyuma <ebuzyuma@gmail.com>
2024-12-21 11:40:50 +0100

    Day 21 solution

--- AdventOfCode2024/Day21/day21.mjs
+++ AdventOfCode2024/Day21/day21.mjs
@@ -1,5 +1,5 @@
 import * as utils from "../utils.mjs";
-import { dirname } from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 const scriptDirectory = dirname(fileURLToPath(import.meta.url));
 const exampleInput = utils.readInput(scriptDirectory, "example.txt");
@@ -35,8 +35,8 @@ function solve(input) {
     }
 
     if (a === b) {
-      pathCache[key] = [];
-      return [];
+      pathCache[key] = [[]];
+      return [[]];
     }
 
     let aPos, bPos;
@@ -52,82 +52,134 @@ function solve(input) {
       }
     }
 
-    let q = [{ p: aPos, path: [] }];
+    let q = [{ p: aPos, paths: [[]] }];
     let visited = {};
     while (q.length > 0) {
       let next = [];
       for (let q1 of q) {
         let {
           p: [r, c],
-          path,
+          paths,
         } = q1;
-        if (r === bPos[0] && c === bPos[1]) {
-          pathCache[key] = path;
-          return path;
-        }
         visited[`${r}x${c}`] = true;
         let n = Object.entries(dirs)
           .map(([dir, [dr, dc]]) => [dir, [r + dr, c + dc, dir]])
           .filter(([dir, [r, c]]) => r >= 0 && r < pad.length && c >= 0 && c < pad[0].length)
           .filter(([dir, [r, c]]) => !visited[`${r}x${c}`])
           .filter(([dir, [r, c]]) => pad[r][c] !== " ")
-          .map(([dir, [r, c]]) => ({ p: [r, c], path: [...path, dir] }));
+          .map(([dir, [r, c]]) => ({ p: [r, c], paths: paths.map((path) => [...path, dir]) }));
 
         next.push(...n);
       }
 
+      const match = next.filter(({ p: [r, c] }) => r === bPos[0] && c === bPos[1]);
+      if (match.length > 0) {
+        const result = match.flatMap(({ paths }) => paths);
+        pathCache[key] = result;
+        return result;
+      }
+
       q = next;
     }
-
-    // const path = [];
-    // const dr = bPos[0] - aPos[0];
-    // const dc = bPos[1] - aPos[1];
-    // for (let i = 0; i > dr; i--) {
-    //   path.push("^");
-    // }
-
-    // for (let i = 0; i < dc; i++) {
-    //   path.push(">");
-    // }
-
-    // for (let i = 0; i < dr; i++) {
-    //   path.push("v");
-    // }
-
-    // for (let i = 0; i > dc; i--) {
-    //   path.push("<");
-    // }
-
-    // pathCache[key] = path;
-    // return path;
   };
   const type = (pad, code) => {
-    let fullPath = [];
+    let fullPath = [""];
     code = "A" + code;
     for (let i = 0; i < code.length - 1; i++) {
       const sub = findPath(pad, code[i], code[i + 1]);
-      fullPath.push(...sub, "A");
+      fullPath = fullPath.flatMap((path) => sub.map((s) => path + s.join("") + "A"));
     }
 
-    return fullPath.join("");
+    return fullPath;
   };
 
   // Part 1
   let p1 = 0;
   for (let code of input) {
-    console.log(Object.keys(pathCache).length);
     const t1 = type(numPad, code);
-    const t2 = type(dirPad, t1);
-    const t3 = type(dirPad, t2);
+    let t2 = [];
+    for (let t of t1) {
+      const tt = type(dirPad, t);
+      t2.push(...tt);
+    }
+    const best2 = Math.min(...new Set(t2.map((x) => x.length)));
+    t2 = t2.filter((x) => x.length === best2);
+    let best3 = Number.MAX_VALUE;
+    for (let t of t2) {
+      const tt = type(dirPad, t);
+      best3 = Math.min(best3, ...new Set(tt.map((x) => x.length)));
+    }
+
     const numCode = parseInt(code.substring(0, code.length - 1));
-    p1 += t3.length * numCode;
+    p1 += best3 * numCode;
   }
 
   // Part 2
-  let p2 = 0;
+  const rCache = {};
+  const findBest = (a, b, n) => {
+    const key = `${a}x${b}x${n}`;
+    // console.log(key);
+    if (rCache[key]) {
+      return rCache[key];
+    }
+
+    const paths = pathCache[`${a}x${b}`];
+    if (n === 0) {
+      rCache[key] = paths[0].length + 1;
+      return rCache[key];
+    }
+
+    if (paths[0].length === 0) {
+      // Staying on same key
+      rCache[key] = 1;
+      return 1;
+    }
+
+    let best = Number.MAX_VALUE;
+    for (let path of paths) {
+      let pBest = findBest("A", path[0], n - 1) + findBest(path.last(), "A", n - 1);
+      for (let i = 0; i < path.length - 1; i++) {
+        pBest += findBest(path[i], path[i + 1], n - 1);
+      }
+
+      best = Math.min(best, pBest);
+    }
+
+    rCache[key] = best;
+    return best;
+  };
+
+  const type2 = (pad, code, n) => {
+    let fullPath = 0;
+    code = "A" + code;
+    for (let i = 0; i < code.length - 1; i++) {
+      const sub = findBest(code[i], code[i + 1], n);
+      fullPath += sub;
+    }
+
+    return fullPath;
+  };
+  const run = (input, nRobots) => {
+    let complexity = 0;
+    for (let code of input) {
+      // console.log("cracking", code);
+      const t1 = type(numPad, code);
+      const t2 = [];
+      for (let t of t1) {
+        const tt = type2(dirPad, t, nRobots);
+        t2.push(tt);
+      }
+
+      const shortest = Math.min(...t2);
+      const numCode = parseInt(code.substring(0, code.length - 1));
+      complexity += shortest * numCode;
+    }
+    return complexity;
+  };
+  const p2 = run(input, 25 - 1);
 
   return [p1, p2];
 }
-// 180184
+
 console.log("example:", solve(exampleInput));
 console.log(" puzzle:", solve(puzzleInput));
